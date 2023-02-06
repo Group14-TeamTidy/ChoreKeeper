4430b38562fb1b52cb21477365048682c0476b5a
Joshua Dueck <joshua.dueck@gmail.com>
2023-02-06 15:49:45 -0600

    Added redirect to signup page, like login page

--- frontend/src/components/pages/SignUp.js
+++ frontend/src/components/pages/SignUp.js
@@ -1,5 +1,5 @@
 import { React } from "react";
-import { useNavigate } from "@tanstack/react-location";
+import { Navigate, useNavigate } from "@tanstack/react-location";
 import AuthService from "../../services/AuthService";
 
 const SignUp = () => {
@@ -13,32 +13,36 @@ const SignUp = () => {
     });
   };
 
-  return (
-    <>
-      <div>
-        <h1>Sign Up</h1>
-      </div>
-      <div>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <label>
-            Email:
-            <input type="email" name="email" />
-          </label>
-          <br />
-          <label>
-            Password:
-            <input
-              type="password"
-              name="password"
-              autoComplete="new-password"
-            />
-          </label>
-          <br />
-          <input type="submit" value="Sign Up!" />
-        </form>
-      </div>
-    </>
-  );
+  if (AuthService.getCurrentUser() && AuthService.getToken()) {
+    return <Navigate to="/" />;
+  } else {
+    return (
+      <>
+        <div>
+          <h1>Sign Up</h1>
+        </div>
+        <div>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <label>
+              Email:
+              <input type="email" name="email" />
+            </label>
+            <br />
+            <label>
+              Password:
+              <input
+                type="password"
+                name="password"
+                autoComplete="new-password"
+              />
+            </label>
+            <br />
+            <input type="submit" value="Sign Up!" />
+          </form>
+        </div>
+      </>
+    );
+  }
 };
 
 export default SignUp;
