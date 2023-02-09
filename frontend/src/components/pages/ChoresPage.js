af157552aae8687b0823609bb4f7d73f1d8a6eda
Kim <kimberlyawills@hotmail.com>
2023-02-09 17:33:25 -0600

    Added title, moved table to center, moved menu to right

--- frontend/src/components/pages/ChoresPage.js
+++ frontend/src/components/pages/ChoresPage.js
@@ -14,12 +14,13 @@ const ChoresPage = () => {
   //   axios.get(`${process.env.REACT_APP_API_BASE_URL}`).then((res) => res.data)
   // );
 
+  // Dropdown menu
   const [open, setOpen] = useState(false);
   const DropdownMenuItem = (props) => {
     return (
-      <button className="dropdownItem" onClick={props.handler}>
-        {props.text}
-      </button>
+      <li>
+        <button className="dropdownItem" onClick={props.handler}>{props.text}</button>
+      </li>
     );
   };
 
@@ -30,46 +31,46 @@ const ChoresPage = () => {
 
   const columns = useMemo(() =>
     [
-        {
-            Header: 'Name',
-            accessor: 'name',
-        },
-        {
-            Header: 'Frequency',
-            accessor: 'freq',
-        },
-        {
-            Header: 'Location',
-            accessor: 'loc',
-        },
-        {
-            Header: 'Duration',
-            accessor: 'dur',
-        },
-        {
-            Header: 'Preference',
-            accessor: 'pref',
-        },
+      {
+        Header: 'Name',
+        accessor: 'name',
+      },
+      {
+        Header: 'Frequency',
+        accessor: 'freq',
+      },
+      {
+        Header: 'Location',
+        accessor: 'loc',
+      },
+      {
+        Header: 'Duration',
+        accessor: 'dur',
+      },
+      {
+        Header: 'Preference',
+        accessor: 'pref',
+      },
     ],
     []
   )
 
   const data = useMemo(
     () => [
-        {
-            name: 'Sweep',
-            freq: 'Every 1 week',
-            loc: 'Kitchen',
-            dur: '30 minutes',
-            pref: 'High',
-        },
-        {
-            name: 'Take Out Trash',
-            freq: 'Every 1 week',
-            loc: 'Kitchen',
-            dur: '5 minutes',
-            pref: 'Low',
-        },
+      {
+        name: 'Sweep',
+        freq: 'Every 1 week',
+        loc: 'Kitchen',
+        dur: '30 minutes',
+        pref: 'High',
+      },
+      {
+        name: 'Take Out Trash',
+        freq: 'Every 1 week',
+        loc: 'Kitchen',
+        dur: '5 minutes',
+        pref: 'Low',
+      },
     ],
     []
   )
@@ -89,62 +90,57 @@ const ChoresPage = () => {
     return <Navigate to="/login" />;
   } else {
     return (
-      <div>
-        <div className="App">
-          <p>Chores Page</p>
-          <p>Hello, {currentUser.email}</p>
-          <p>Your token is: {JSON.stringify(token)}</p>
-        </div>
-
-        <div id="menuContainer">
-          <button onClick={() => setOpen(!open)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"></path>
-            </svg>
-          </button>
-          <div id="dropdownMenu" className={open ? "" : "hidden"}>
-            <div>
-              <p>Account</p>
-              <p>{currentUser.email}</p>
-            </div>
-            <DropdownMenuItem text={"Manage Account"}/>
-            <DropdownMenuItem text={"Log Out"} handler={handleLogout}/>
+      <div className="App">
+        <div class="header">
+          <div id="dropdownMenuContainer">
+            <button onClick={() => setOpen(!open)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"></path>
+              </svg>
+            </button>
+            <ul id="dropdownMenu" className={open ? "" : "hidden"}>
+              <li>{currentUser.email}</li>
+              <DropdownMenuItem text={"Manage Account"}/>
+              <DropdownMenuItem text={"Log Out"} handler={handleLogout}/>
+            </ul>
           </div>
+
+          <h1>Chore Keeper</h1>
         </div>
         
-        <button>
-          New Chore
-        </button>
+        <div className="content">
+          <button>New Chore</button>
 
-        <table id="choresList" {...getTableProps()}>
+          <table id="choresList" className="center" {...getTableProps()}>
             <thead>
-                {headerGroups.map(headerGroup => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map(column => (
-                            <th {...column.getHeaderProps()}>
-                                {column.render('Header')}
-                            </th>
-                        ))}
-                    </tr>
-                ))}
+              {headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map(column => (
+                    <th {...column.getHeaderProps()}>
+                      {column.render('Header')}
+                    </th>
+                  ))}
+                </tr>
+              ))}
             </thead>
             <tbody {...getTableBodyProps()}>
-                {rows.map(row => {
-                    prepareRow(row)
-                    return (
-                        <tr {...row.getRowProps()}>
-                            {row.cells.map(cell => {
-                                return (
-                                    <td {...cell.getCellProps()}>
-                                        {cell.render('Cell')}
-                                    </td>
-                                )
-                            })}
-                        </tr>
-                    )
-                })}
+              {rows.map(row => {
+                prepareRow(row)
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map(cell => {
+                      return (
+                        <td {...cell.getCellProps()}>
+                          {cell.render('Cell')}
+                        </td>
+                      )
+                    })}
+                  </tr>
+                )
+              })}
             </tbody>
-        </table>
+          </table>
+        </div>
       </div>
     );
   }
