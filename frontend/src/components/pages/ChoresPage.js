b083a0328adbab06e28c07287faf152c76745c22
Kim <kimberlyawills@hotmail.com>
2023-02-08 19:29:14 -0600

    Make dropdown menu toggleable

--- frontend/src/components/pages/ChoresPage.js
+++ frontend/src/components/pages/ChoresPage.js
@@ -1,5 +1,6 @@
 import { React } from "react";
 import { Navigate, useNavigate } from "@tanstack/react-location";
+import { useState } from "react";
 // import { useQuery } from "react-query";
 import AuthService from "../../services/AuthService";
 // import axios from "axios";
@@ -10,6 +11,8 @@ const ChoresPage = () => {
   // const { isLoading, error, data } = useQuery("repoData", () =>
   //   axios.get(`${process.env.REACT_APP_API_BASE_URL}`).then((res) => res.data)
   // );
+
+  const [open, setOpen] = useState(false);
 
   const handleLogout = () => {
     AuthService.logout();
@@ -38,14 +41,13 @@ const ChoresPage = () => {
           <p>Your token is: {JSON.stringify(token)}</p>
         </div>
 
-        {/*TODO: Make the dropdown menu toggleable */}
         <div id="menuContainer">
-          <button>
+          <button onClick={() => setOpen(!open)}>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"></path>
             </svg>
           </button>
-          <div id="dropdownMenu">
+          <div id="dropdownMenu" className={open ? "" : "hidden"}>
             <div>
               <p>Account</p>
               <p>{currentUser.email}</p>
@@ -55,7 +57,7 @@ const ChoresPage = () => {
           </div>
         </div>
 
-        <br/><br/> {/* TODO: remove these when I do the CSS */}
+        <br/> {/* TODO: remove when CSS is added */}
         <button>
           New Chore
         </button>
