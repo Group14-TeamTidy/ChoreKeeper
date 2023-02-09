1e945b87092bb0a4c7eb7f2b35ec44fcb4f23041
SyedAli72 <sjafry@lotlinx.com>
2023-02-08 18:21:58 -0600

    Routes for chores defined. create chore functionality for backend completed.

--- server/controller/chore.js
+++ server/controller/chore.js
@@ -10,13 +10,29 @@ import Chore from "../models/Chore.js";
  */
  export const createChore = async (req, res) => {
     // Sanitize data
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
       }
 
     try {
-        const { name, frequency, location, duration, preference } = req.body;
-        console.log(name);
+        const { name, frequency, location, duration, preference } = req.body; //extracting fields recieved from request
+        // console.log(req.body);
+        console.log(`Task Name: ${name},\nFrequency Quantity & Interval: ${frequency.quantity} times every ${frequency.interval},\nLocation: ${location},\nDuration (s): ${duration},\nPreference: ${preference}`);
+
+        // check if this chore already exists
+        const existingChore = await Chore.findOne({ name });
+        if (existingChore) {
+            return res.status(409).json({ message: `Chore: ${name} already exists. If you want to change somthing, please use the EDIT option.` });
+        }
+
+        // creating a new chore to add to database
+        const newChore = new Chore({name, frequency, location, duration, preference});
+
+        // Save to database
+        const savedChore = await newChore.save();
+
+        return res.status(201).json({ Chore: savedChore });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: "Chore could not be created" });
--- server/routes/chore.js
+++ server/routes/chore.js
@@ -1,16 +1,16 @@
 import express from "express";
 
-import { getUser } from "../controller/user.js";
+import { createChore } from "../controller/chore.js";
 
 const router = express.Router();
 
 //ROUTES
-router.get("/chores/id", ); //get a single chore
+router.get("/id", ); //get a single chore
 
-router.post("/chores", ); //create a chore
+router.post("/create", createChore); //create a chore
 
-router.post("/chores/id", ); //edit a chore
+router.post("/id", ); //edit a chore
 
-router.delete("/chores/id", ); //delete a chore
+router.delete("/id", ); //delete a chore
 
 export default router;
