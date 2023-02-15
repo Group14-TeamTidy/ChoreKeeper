ddd289ee005e2545c4130e30bead1a51d10c59cd
SyedAli72 <sjafry@lotlinx.com>
2023-02-14 18:35:38 -0600

    renamed create route name to match requested route

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ export default function makeApp(database) {
   // add routes here
 
   app.use("/api/user", userRoute);
-  app.use("/api/chore", choreRoute);
+  app.use("/api/chores", choreRoute);
 
   app.post("/api/login", login);
   app.post("/api/signup", register);
