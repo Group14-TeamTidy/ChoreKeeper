1ebb32fada48a506e8ae3d20bc16ec1a6f877b73
SyedAli72 <sjafry@lotlinx.com>
2023-02-14 22:27:35 -0600

    Added delete chores method and unit tests for delete chores.

--- server/controller/chore.js
+++ server/controller/chore.js
@@ -57,6 +57,7 @@ export const createChore = async (req, res) => {
 
     //add the new chore to the list of chores that the user has created
     user.chores.push(savedChore._id);
+    await user.save();
 
     return res.status(201).json({ Chore: savedChore });
   } catch (error) {
@@ -86,7 +87,7 @@ export const getAllChores = async (req, res) => {
 
     // Get the list of chores for the user
     const chores = await Promise.all(
-      user.choreList.map((id) => Chore.findById(id))
+      user.chores.map((id) => Chore.findById(id))
     );
 
     // Format the chore list for the response
@@ -157,3 +158,39 @@ export const editChore = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 }; //editChore
+
+ /*
+ ** This function deletes a chore from the chore database as well as user chore list
+ ** @param {Object} req - The request object
+ ** @param {Object} res - The response object
+ */
+export const deleteChore = async (req, res) => {
+  try{
+    const choreId = req.params.id;
+    // delete chore from chores database
+    await Chore.findByIdAndDelete({ _id: choreId });
+
+    // get the user that is creating the chore
+    const userID = req.user.id;
+    const user = await User.findOne({ _id: userID }); // search for the user
+
+    // remove chore id from the users chore array
+    userChores = user.chores;
+    const idIndex = array.indexof(choreId);
+    if (idIndex > -1) { // only splice array when id is found
+      array.splice(idIndex, 1);
+      // assigning the updated array to user.chores
+      user.chores = userChores; 
+      await user.save();
+      return res.status(200).json({ message: `Chore with id ${choreId} deleted successfully!`});
+    }
+    else {
+      console.log("Chore ID not found in user's chore list!")
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  }catch (error) {
+    console.error(error);
+    // Return an error message in the response in case of any unexpected errors
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}; //deleteChore
