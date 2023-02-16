a71a7ccd6b86d0ea89633ba81580941117ba142b
Kim <kimberlyawills@hotmail.com>
2023-02-16 16:21:26 -0600

    Styled the edit, delete, and arrow buttons

--- frontend/src/components/pages/ChoresPage.js
+++ frontend/src/components/pages/ChoresPage.js
@@ -155,12 +155,15 @@ const ChoresPage = () => {
         Cell: ({value}) => {
           if(value !== -1) {
             return (
-              <button onClick={() => {
+              <button className="editButton" onClick={() => {
                 let chore = chores.find((val) => {return value === val._id});
                 setCurrChore(chore);
                 handleShow();
               }}>
-                Edit
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                  <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                </svg>
               </button>
             )
           } else {
@@ -174,12 +177,14 @@ const ChoresPage = () => {
         Cell: ({value}) => {
           if(value !== -1) {
             return (
-              <button onClick={() => {
+              <button className="deleteButton" onClick={() => {
                 let chore = chores.find((val) => {return value === val._id});
                 setCurrChore(chore);
                 handleDeleteModalShow();
               }}>
-                Delete
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+                  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+                </svg>
               </button>
             )
           } else {
@@ -312,12 +317,20 @@ const ChoresPage = () => {
             </table>
 
             <div id="paginationButtons">
-              {canPreviousPage && <Button onClick={() => previousPage()}>{'<'}</Button>}
+              {canPreviousPage && <Button onClick={() => previousPage()}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="0.75em" height="1.5em" fill="currentColor" className="bi bi-chevron-left" viewBox="0 0 16 16">
+                  <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                </svg>
+              </Button>}
               {pageCount > 1 && pageCount >= setPageButton(1, pageIndex) && <Button className={setPageButton(1, pageIndex) === pageIndex + 1 ? "currentPage" : ""} onClick={() => {gotoPage(setPageButton(1, pageIndex) - 1)}}>{setPageButton(1, pageIndex)}</Button>}
               {pageCount > 1 && pageCount >= setPageButton(2, pageIndex) && <Button className={setPageButton(2, pageIndex) === pageIndex + 1 ? "currentPage" : ""} onClick={() => {gotoPage(setPageButton(2, pageIndex) - 1)}}>{setPageButton(2, pageIndex)}</Button>}
               {pageCount >= setPageButton(3, pageIndex) && <Button className={setPageButton(3, pageIndex) === pageIndex + 1 ? "currentPage" : ""} onClick={() => {gotoPage(setPageButton(3, pageIndex) - 1)}}>{setPageButton(3, pageIndex)}</Button>}
               {pageCount >= setPageButton(4, pageIndex) && <Button className={setPageButton(4, pageIndex) === pageIndex + 1 ? "currentPage" : ""} onClick={() => {gotoPage(setPageButton(4, pageIndex) - 1)}}>{setPageButton(4, pageIndex)}</Button>}
-              {canNextPage && <Button onClick={() => nextPage()} disabled={!canNextPage}>{'>'}</Button>}
+              {canNextPage && <Button onClick={() => nextPage()} disabled={!canNextPage}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="0.75em" height="1.5em" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
+                  <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                </svg>
+              </Button>}
             </div>
           </div>
         </div>
