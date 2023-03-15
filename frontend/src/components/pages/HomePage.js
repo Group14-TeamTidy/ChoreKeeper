0b6ea640f8dd4fd2bb6eadde3f394ae9e1d51afa
Kim <kimberlyawills@hotmail.com>
2023-03-15 13:35:29 -0500

    Added toggleable notification button

--- frontend/src/components/pages/HomePage.js
+++ frontend/src/components/pages/HomePage.js
@@ -19,6 +19,11 @@ const HomePage = () => {
   const token = AuthService.getToken();
   let displayChoresPage = window.location.pathname === "/chores";
 
+  const [receiveNotifs, setNotifs] = useState(false);
+  const handleNotifs = () => {
+    return false;
+  }
+
   // Logs out the user
   const handleLogout = () => {
     AuthService.logout();
@@ -119,6 +124,23 @@ const HomePage = () => {
               }}
             >
               <span className="pi pi-plus"></span>&nbsp;Chore
+            </Button>
+
+            <Button
+              id="toggleNotifs"
+              onClick={() => {
+                setNotifs(!receiveNotifs);
+              }}
+            >
+              {receiveNotifs ? 
+                <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+                 <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
+                </svg>
+                :
+                <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-bell-slash-fill" viewBox="0 0 16 16">
+                  <path d="M5.164 14H15c-1.5-1-2-5.902-2-7 0-.264-.02-.523-.06-.776L5.164 14zm6.288-10.617A4.988 4.988 0 0 0 8.995 2.1a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 7c0 .898-.335 4.342-1.278 6.113l9.73-9.73zM10 15a2 2 0 1 1-4 0h4zm-9.375.625a.53.53 0 0 0 .75.75l14.75-14.75a.53.53 0 0 0-.75-.75L.625 15.625z"/>
+                </svg>
+              }
             </Button>
 
             <div id="dropdownMenuContainer">
